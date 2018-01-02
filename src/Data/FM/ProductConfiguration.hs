3f40f9f61f71e30f42bc3f9f017f9ae460e9f89e
thi4go <thiagofigueiredo777@gmail.com>
2018-01-02 19:37:08 -0200

    PC model

--- src/Data/FM/ProductConfiguration.hs
+++ src/Data/FM/ProductConfiguration.hs
@@ -10,25 +10,59 @@ import Data.FM.Tree
 import Data.FM.FeatureModel
 
 
-data Product = Product {
-    _productFTree :: FeatureTree,
-    _productFList :: [Feature]
-} deriving (Show, Eq)
-makeLenses ''Product
+-- data Product = Product {
+--   _productList :: [Feature],
+--   _productTree :: FeatureTree
+-- } deriving (Show, Eq)
+-- makeLenses ''Product
+
 
 data ProductConfiguration = ProductConfiguration {
-    _featureModel   :: FeatureModel,
-    _product        :: Product,
-    _valid          :: Bool
+  _featureModel      :: FeatureModel,
+  _productDerivation :: [Feature],
+  _valid             :: Bool
 } deriving (Show, Eq)
 makeLenses ''ProductConfiguration
 
 
-prod = Product derivTree (flatten derivTree)
+prod       = Product derivTree (flatten derivTree)
 
-eval :: ProductConfiguration -> Bool
-eval _ = False
+prodConfig = ProductConfiguration fm02 ["iris", "security", "RSA", "persist"]
 
+
+ruleChecker :: ProductConfiguration -> [FeatureExp]
+ruleChecker pc = view featureTree $ view featureModel pc >>= \fmtree ->
+                 view productf pc                        >>= \ps ->
+                 getProductExpr fmtree pc
+
+
+getProductExpr :: FeatureTree -> [Feature] -> [FeatureExp]
+getProductExpr (Node f []) fs     = []
+
+getProductExpr (Node f (x:xs)) fs =
+  (checkTypeRules f fs) ++
+  (checkGroupRules f (x:xs) fs) ++
+  (map (\x -> getProductExpr x) (x:xs))
+
+
+
+checkTypeRules :: Feature -> [Feature] -> FeatureExp
+checkTypeRules f fs =
+  case (view typeF f) of
+    Mandatory -> if n@(view name f) `elem` fs then (Ref n) else Not (Ref n)
+    Optional  ->
+
+
+checkGroupRules :: Feature -> [Feature] -> FeatureExp
+checkGroupRules f fs =
+  case (view group f) of
+
+
+
+
+
+
+-- (map (\x -> getProductExpr x) (x:xs))
 ft02 = Node (Feature "iris" BasicFeature Mandatory) [
              (Node (Feature "security" OrFeature Mandatory) [
                 (Node (Feature "sha-256" BasicFeature Optional) []),
