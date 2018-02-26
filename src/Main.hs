1d6aa664535b8df2639453a97ad8a4ac7295beb4
Thiago de Freitas Figueiredo <thiagofigueiredo777@gmail.com>
2018-02-26 16:58:11 -0300

    building lexer for xml parser

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,7 @@ import Text.Parsec.String
 
 import Parser.Parsec 
 
-main = parseFromFile parseFIDE "fm.ide" >>= \result ->
+main = parseFromFile parseReservedNames "fm.ide" >>= \result ->
        case result of
          Left err -> print err
          Right fm -> print (show fm) 
--- src/Parser/Parsec.hs
+++ src/Parser/Parsec.hs
@@ -1,14 +1,58 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Parser.Parsec where
 
 import Data.Tree
 import Text.Parsec
 import Text.Parsec.String
+import Text.Parsec.Language
+import Text.Parsec.Token
 
 import Data.FM.FeatureModel
 import Data.FM.Tree
 import Data.FM.Feature
 import Data.FM.Expression
 
+
+xmlFMDef :: LanguageDef st
+xmlFMDef = LanguageDef
+          { commentStart    = ""
+          , commentEnd      = ""
+          , commentLine     = ""
+          , nestedComments  = False
+          , identStart      = letter <|> char '_'
+          , identLetter     = alphaNum <|> oneOf "_'"
+          , opStart         = opLetter emptyDef
+          , opLetter        = oneOf ""
+          , reservedOpNames = []
+          , reservedNames   = ["<featureModel", "<struct>", 
+                               "<and", "<alt", "<or",
+                               "<feature", "<constraints>",
+                               "<rule>", "<var>", "<eq>", 
+                               "<imp>"]
+          , caseSensitive   = True
+          }
+
+
+lexer = makeTokenParser xmlFMDef
+
+parseReservedNames = reserved lexer
+parseString        = stringLiteral lexer
+
+main = parseFromFile parseFIDE "fm.ide" >>= \result ->
+       case result of
+         Left err -> print err
+         Right fm -> print (show fm) 
+  
+
+         
+parseFIDE :: Parsec String () String
+parseFIDE =
+  do
+    s      <- parseString
+    result <- parseReservedNames s
+    return (s)         
+{- 
 parseFIDE :: Parsec String () FeatureModel
 parseFIDE =
   string "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>" >> blanks >> 
@@ -21,7 +65,7 @@ parseFIDE =
   string "<featureOrder userDefined=\"false\"/>" >> blanks >>  
   string "</featureModel>" >> blanks >> 
   return (FeatureModel ft [])
-
+-}
 
 parseFeatureTree :: Parsec String () FeatureTree
 parseFeatureTree =
@@ -33,6 +77,11 @@ parseFeatureTree =
 
 blanks = many (space <|> newline) 
 
+
+parseFromFileLocal p fname = 
+  do
+    input <- readFile fname
+    return (runParser p () fname input)
 -- parseFeatureType :: Parsec String () FeatureType
 -- parseFeatureType =
 --   many1 letter >>= \t ->
