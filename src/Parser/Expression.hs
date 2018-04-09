1feaf5c931cd6a8e7c267594e2a215ee3c866c7c
Thiago de Freitas Figueiredo <thiagofigueiredo777@gmail.com>
2018-04-09 17:23:58 -0300

    expression parser from CK to exp type

---
+++ src/Parser/Expression.hs
@@ -0,0 +1,45 @@
+module Parser.Expression where
+
+import Text.Parsec
+import Text.Parsec.String
+
+import Data.FM.Expression
+
+
+parseExpression :: Parsec String () FeatureExp
+parseExpression =
+  parseExpNot   <|>
+  parseExpAnd   <|>
+  parseExpOr    <|>
+  parseExpTrue  <|>
+  parseExpFalse <|>
+  parseExpRef
+
+
+parseExpNot :: Parsec String () FeatureExp
+parseExpNot =
+  string "Not(" >> parseExpression >>= \e1 -> string ")" >> return (Not e1)
+
+parseExpAnd :: Parsec String () FeatureExp
+parseExpAnd =
+  string "And(" >> parseExpression >>= \e1 -> many space >> string ","
+  >> many space >> parseExpression >>= \e2 -> string ")" >>
+  return (And e1 e2)
+
+parseExpOr :: Parsec String () FeatureExp
+parseExpOr =
+  string "Or("  >> parseExpression >>= \e1 -> many space >> string ","
+  >> many space >> parseExpression >>= \e2 -> string ")" >>
+  return (Or e1 e2)
+
+parseExpTrue :: Parsec String () FeatureExp
+parseExpTrue =
+  string "True" >> return (B True)
+
+parseExpFalse :: Parsec String () FeatureExp
+parseExpFalse =
+  string "False" >> return (B False)
+
+parseExpRef :: Parsec String () FeatureExp
+parseExpRef =
+  letter >> many (alphaNum <|> space) >>= \r -> return (Ref r)
